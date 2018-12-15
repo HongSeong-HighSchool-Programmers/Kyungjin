5c8e0c9713072032e43bfce3d6934a75ebf7ce2a
kyungjinMin <36812545+kyungjinMin@users.noreply.github.com>
2018-12-15 22:35:32 +0900

    Create 단어장.vb

---
+++ 단어장.vb
@@ -0,0 +1,49 @@
+Public Class MAIN
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles E_B.Click
+        E_L.Show()
+    End Sub
+
+    Private Sub Label4_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Label5_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles S1_B.Click
+        S1_L.Show()
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles S2_B.Click
+        S2_L.Show()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles C_B.Click
+        C_L.Show()
+    End Sub
+
+    Private Sub J_B_Click(sender As Object, e As EventArgs) Handles J_B.Click
+        J_L.Show()
+    End Sub
+
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles J_T_B.Click
+        J_T.Show()
+    End Sub
+
+    Private Sub E_T_B_Click(sender As Object, e As EventArgs) Handles E_T_B.Click
+        E_T.Show()
+    End Sub
+
+    Private Sub C_T_B_Click(sender As Object, e As EventArgs) Handles C_T_B.Click
+        C_T.Show()
+    End Sub
+
+    Private Sub MK_B_Click(sender As Object, e As EventArgs) Handles MK_B.Click
+        MK.Show()
+    End Sub
+End Class
