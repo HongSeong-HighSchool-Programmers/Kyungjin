3c7914a33b024c4334e3e4d2da5f8a81683ccaf1
kyungjinMin <36812545+kyungjinMin@users.noreply.github.com>
2018-12-15 22:34:57 +0900

    Create 단어장.Designer.vb

---
+++ 단어장.Designer.vb
@@ -0,0 +1,295 @@
+<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class MAIN
+    Inherits System.Windows.Forms.Form
+
+    'Form은 Dispose를 재정의하여 구성 요소 목록을 정리합니다.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows Form 디자이너에 필요합니다.
+    Private components As System.ComponentModel.IContainer
+
+    '참고: 다음 프로시저는 Windows Form 디자이너에 필요합니다.
+    '수정하려면 Windows Form 디자이너를 사용하십시오.  
+    '코드 편집기에서는 수정하지 마세요.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.S1_B = New System.Windows.Forms.Button()
+        Me.S2_B = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.Label14 = New System.Windows.Forms.Label()
+        Me.Label15 = New System.Windows.Forms.Label()
+        Me.J_B = New System.Windows.Forms.Button()
+        Me.C_B = New System.Windows.Forms.Button()
+        Me.E_B = New System.Windows.Forms.Button()
+        Me.E_T_B = New System.Windows.Forms.Button()
+        Me.C_T_B = New System.Windows.Forms.Button()
+        Me.J_T_B = New System.Windows.Forms.Button()
+        Me.MK_B = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'S1_B
+        '
+        Me.S1_B.Font = New System.Drawing.Font("굴림", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.S1_B.ForeColor = System.Drawing.Color.Red
+        Me.S1_B.Location = New System.Drawing.Point(167, 414)
+        Me.S1_B.Name = "S1_B"
+        Me.S1_B.Size = New System.Drawing.Size(63, 45)
+        Me.S1_B.TabIndex = 4
+        Me.S1_B.Text = "★"
+        Me.S1_B.UseVisualStyleBackColor = True
+        '
+        'S2_B
+        '
+        Me.S2_B.Font = New System.Drawing.Font("굴림", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.S2_B.ForeColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.S2_B.Location = New System.Drawing.Point(143, 477)
+        Me.S2_B.Name = "S2_B"
+        Me.S2_B.Size = New System.Drawing.Size(110, 45)
+        Me.S2_B.TabIndex = 13
+        Me.S2_B.Text = "★★" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        Me.S2_B.UseVisualStyleBackColor = True
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("굴림", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.Label4.ForeColor = System.Drawing.Color.Red
+        Me.Label4.Location = New System.Drawing.Point(12, 28)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(57, 40)
+        Me.Label4.TabIndex = 14
+        Me.Label4.Text = "★"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Lucida Bright", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.ForeColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.Label5.Location = New System.Drawing.Point(53, 28)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(58, 45)
+        Me.Label5.TabIndex = 15
+        Me.Label5.Text = "W"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Lucida Bright", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.ForeColor = System.Drawing.Color.Gold
+        Me.Label8.Location = New System.Drawing.Point(97, 28)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(52, 45)
+        Me.Label8.TabIndex = 16
+        Me.Label8.Text = "O"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Font = New System.Drawing.Font("Lucida Bright", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label9.ForeColor = System.Drawing.Color.Lime
+        Me.Label9.Location = New System.Drawing.Point(145, 28)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(49, 45)
+        Me.Label9.TabIndex = 17
+        Me.Label9.Text = "R"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Font = New System.Drawing.Font("Lucida Bright", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label10.ForeColor = System.Drawing.Color.Aqua
+        Me.Label10.Location = New System.Drawing.Point(191, 28)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(52, 45)
+        Me.Label10.TabIndex = 18
+        Me.Label10.Text = "D"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Font = New System.Drawing.Font("Lucida Bright", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label11.ForeColor = System.Drawing.Color.Blue
+        Me.Label11.Location = New System.Drawing.Point(159, 73)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(43, 45)
+        Me.Label11.TabIndex = 19
+        Me.Label11.Text = "L"
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Font = New System.Drawing.Font("Lucida Bright", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label12.ForeColor = System.Drawing.Color.Fuchsia
+        Me.Label12.Location = New System.Drawing.Point(196, 73)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(34, 45)
+        Me.Label12.TabIndex = 20
+        Me.Label12.Text = "I"
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Font = New System.Drawing.Font("Lucida Bright", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label13.ForeColor = System.Drawing.Color.Red
+        Me.Label13.Location = New System.Drawing.Point(224, 73)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(42, 45)
+        Me.Label13.TabIndex = 21
+        Me.Label13.Text = "S"
+        '
+        'Label14
+        '
+        Me.Label14.AutoSize = True
+        Me.Label14.Font = New System.Drawing.Font("Lucida Bright", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label14.ForeColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.Label14.Location = New System.Drawing.Point(259, 73)
+        Me.Label14.Name = "Label14"
+        Me.Label14.Size = New System.Drawing.Size(48, 45)
+        Me.Label14.TabIndex = 22
+        Me.Label14.Text = "T"
+        '
+        'Label15
+        '
+        Me.Label15.AutoSize = True
+        Me.Label15.Font = New System.Drawing.Font("굴림", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.Label15.ForeColor = System.Drawing.Color.Gold
+        Me.Label15.Location = New System.Drawing.Point(297, 76)
+        Me.Label15.Name = "Label15"
+        Me.Label15.Size = New System.Drawing.Size(57, 40)
+        Me.Label15.TabIndex = 23
+        Me.Label15.Text = "★"
+        '
+        'J_B
+        '
+        Me.J_B.BackgroundImage = Global.단어장_만들기_프로젝트.My.Resources.Resources.목록_이미지
+        Me.J_B.Location = New System.Drawing.Point(294, 332)
+        Me.J_B.Name = "J_B"
+        Me.J_B.Size = New System.Drawing.Size(45, 51)
+        Me.J_B.TabIndex = 3
+        Me.J_B.UseVisualStyleBackColor = True
+        '
+        'C_B
+        '
+        Me.C_B.BackgroundImage = Global.단어장_만들기_프로젝트.My.Resources.Resources.목록_이미지
+        Me.C_B.Location = New System.Drawing.Point(294, 240)
+        Me.C_B.Name = "C_B"
+        Me.C_B.Size = New System.Drawing.Size(45, 51)
+        Me.C_B.TabIndex = 2
+        Me.C_B.UseVisualStyleBackColor = True
+        '
+        'E_B
+        '
+        Me.E_B.BackgroundImage = Global.단어장_만들기_프로젝트.My.Resources.Resources.목록_이미지
+        Me.E_B.Location = New System.Drawing.Point(294, 143)
+        Me.E_B.Name = "E_B"
+        Me.E_B.Size = New System.Drawing.Size(45, 49)
+        Me.E_B.TabIndex = 1
+        Me.E_B.UseVisualStyleBackColor = True
+        '
+        'E_T_B
+        '
+        Me.E_T_B.Font = New System.Drawing.Font("Lucida Bright", 21.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.E_T_B.Location = New System.Drawing.Point(90, 143)
+        Me.E_T_B.Name = "E_T_B"
+        Me.E_T_B.Size = New System.Drawing.Size(176, 49)
+        Me.E_T_B.TabIndex = 24
+        Me.E_T_B.Text = "English"
+        Me.E_T_B.UseVisualStyleBackColor = True
+        '
+        'C_T_B
+        '
+        Me.C_T_B.Font = New System.Drawing.Font("Britannic Bold", 22.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.C_T_B.Location = New System.Drawing.Point(100, 240)
+        Me.C_T_B.Name = "C_T_B"
+        Me.C_T_B.Size = New System.Drawing.Size(153, 51)
+        Me.C_T_B.TabIndex = 25
+        Me.C_T_B.Text = "中國語"
+        Me.C_T_B.UseVisualStyleBackColor = True
+        '
+        'J_T_B
+        '
+        Me.J_T_B.Font = New System.Drawing.Font("Britannic Bold", 22.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.J_T_B.Location = New System.Drawing.Point(85, 332)
+        Me.J_T_B.Name = "J_T_B"
+        Me.J_T_B.Size = New System.Drawing.Size(181, 51)
+        Me.J_T_B.TabIndex = 26
+        Me.J_T_B.Text = "にほんご"
+        Me.J_T_B.UseVisualStyleBackColor = True
+        '
+        'MK_B
+        '
+        Me.MK_B.Font = New System.Drawing.Font("굴림", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.MK_B.Location = New System.Drawing.Point(350, 576)
+        Me.MK_B.Name = "MK_B"
+        Me.MK_B.Size = New System.Drawing.Size(38, 36)
+        Me.MK_B.TabIndex = 27
+        Me.MK_B.Text = "※"
+        Me.MK_B.UseVisualStyleBackColor = True
+        '
+        'MAIN
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(391, 613)
+        Me.Controls.Add(Me.MK_B)
+        Me.Controls.Add(Me.J_T_B)
+        Me.Controls.Add(Me.C_T_B)
+        Me.Controls.Add(Me.E_T_B)
+        Me.Controls.Add(Me.Label15)
+        Me.Controls.Add(Me.Label14)
+        Me.Controls.Add(Me.Label13)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.S2_B)
+        Me.Controls.Add(Me.S1_B)
+        Me.Controls.Add(Me.J_B)
+        Me.Controls.Add(Me.C_B)
+        Me.Controls.Add(Me.E_B)
+        Me.Cursor = System.Windows.Forms.Cursors.Default
+        Me.Name = "MAIN"
+        Me.Text = "WORD LIST"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents E_B As Button
+    Friend WithEvents C_B As Button
+    Friend WithEvents J_B As Button
+    Friend WithEvents S1_B As Button
+    Friend WithEvents S2_B As Button
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label12 As Label
+    Friend WithEvents Label13 As Label
+    Friend WithEvents Label14 As Label
+    Friend WithEvents Label15 As Label
+    Friend WithEvents E_T_B As Button
+    Friend WithEvents C_T_B As Button
+    Friend WithEvents J_T_B As Button
+    Friend WithEvents MK_B As Button
+End Class
